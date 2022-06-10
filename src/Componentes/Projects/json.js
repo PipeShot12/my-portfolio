0a0870a153aa6fed48c317cc39678ccb18d455ff
Andres <andres1295zz@gmail.com>
2022-06-10 06:09:36 -0500

    improve optimization images src

--- src/Componentes/Projects/json.js
+++ src/Componentes/Projects/json.js
@@ -1,103 +1,103 @@
 const arrayProjects = [
   {
     title: "Todo List App",
-    src: "https://drive.google.com/uc?id=1xOyTNz9IX8FtmkPAtp5yJXsBm5Q3uipL",
+    src: "https://drive.google.com/uc?id=1li0e7ZLSH72fr5VvghmMYOJ47Kw4FZ_S",
     link: "https://todo-list-pipe.netlify.app/",
     alt: "project 1"
   },
   {
     title: "VoTrading Web",
-    src: "https://drive.google.com/uc?id=1xyrsczxiZ_olbg4pVDD7dXzoYQ-tC3aF",
+    src: "https://drive.google.com/uc?id=1ON0tgSNIosGNu2JX7zGI2CYvye786XqQ",
     link: "https://votrading.netlify.app/",
     alt: "project 2"
   },
   {
     title: "Weather App",
-    src: "https://drive.google.com/uc?id=1CAHOG2TzNVsQD1VWgDmTv0VnKyVI0rUM",
+    src: "https://drive.google.com/uc?id=1NeH827hcxppwGCxLR3NYhAXwIC70tvUy",
     link: "https://weather-app-pipe.netlify.app/",
     alt: "project 3"
   },
   {
     title: "Huddle Landing Page",
-    src: "https://drive.google.com/uc?id=1y-XwA98RVqn590FVSqx6OS2uGG0bRnSb",
+    src: "https://drive.google.com/uc?id=1IWD8vRMvjVR_ZvIFntx8rCzOCVnjfgdI",
     link: "https://huddle-landing-page-pipe.netlify.app/",
     alt: "project 4"
   },
   {
     title: "Rock Papper Scissors Game",
-    src: "https://drive.google.com/uc?id=1CcA0x6zRRsv2aer8USTyRHdK98vaGAxn",
+    src: "https://drive.google.com/uc?id=15l-RgkC70W0lV7UJV4KNnSXZSYUhgdqP",
     link: "https://rock-paper-scissors-pipe.netlify.app/",
     alt: "project 5"
   },
   {
     title: "Url Shortener 'Drop'",
-    src: "https://drive.google.com/uc?id=1tkX3gyPRvigyrRm2A0gpDQINnakRuPNm",
+    src: "https://drive.google.com/uc?id=13h7MCSDDbY1JCsB2R4NsN5qxT_oRsFua",
     link: "https://drop-shortener.netlify.app/",
     alt: "project 6"
   },
   {
     title: "Base Apparel Coming Soon",
-    src: "https://drive.google.com/uc?id=1sXMvks5IsXw7_fsLQBOO5Iz2MHRwt1AK",
+    src: "https://drive.google.com/uc?id=1QtpAyfkXsREqhR9Hb51rdWG-PIVKS9KJ",
     link: "https://base-apparel-coming-soon-pipe.vercel.app/",
     alt: "project 7"
   },
   {
     title: "Technical Test for TM",
-    src: "https://drive.google.com/uc?id=1sfmOEXGTviw2k-GU5phj1SEpFIaDusfm",
+    src: "https://drive.google.com/uc?id=1Wcl3pCYTLSG2XEf3MDmq-vmsduXT-Rqa",
     link: "https://prueba-tecnica-kronotime.vercel.app/",
     alt: "project 8"
   },
   {
     title: "Stats Preview Card Component",
-    src: "https://drive.google.com/uc?id=1uwa1grG42Mwuxdal4zvqZ6MaYYrqCxUG",
+    src: "https://drive.google.com/uc?id=1DIa6q0hnMnTrPdNhW5v6xIkXqo098-WB",
     link: "https://stats-preview-card-component-pipe.netlify.app/",
     alt: "project 9"
   },
   {
     title: "Order Summary Card",
-    src: "https://drive.google.com/uc?id=1U9KUG9-WDGeDm9EFXqeF7nbjCqk-v_iM",
+    src: "https://drive.google.com/uc?id=1Sidxa-Fq9Ad-brg-N9DzqqaK8G94C5PQ",
     link: "https://order-summary-card-pipe.netlify.app/",
     alt: "project 10"
   },
   {
     title: "Sign Up Form",
-    src: "https://drive.google.com/uc?id=1XXLWptfoFWRZP6DyDOtFrX5Oamy2-UdY",
+    src: "https://drive.google.com/uc?id=180nvDWqZ7ca-t3ZxpNp0JSwRH58yG_1e",
     link: "https://sign-up-form-pipe.vercel.app/",
     alt: "project 11"
   },
   {
     title: "Ping Single Coming Soon",
-    src: "https://drive.google.com/uc?id=1TNVle6OP4Rxlik1y4VOY6QM8UUK1kv0M",
+    src: "https://drive.google.com/uc?id=1LRkYp0BK_-TpKD1xJGoobMQoMb2LQKPX",
     link: "https://ping-single-coming-soon.netlify.app/",
     alt: "project 12"
   },
   {
     title: "Single Price Grid Component",
-    src: "https://drive.google.com/uc?id=1I4T4JZTvJ68gUiv-G936Qp9GJ-nQqGNF",
+    src: "https://drive.google.com/uc?id=11NuQJl99qIkoM8Hb9sDieWQyzLPqCn0a",
     link: "https://single-price-grid-component-pipe.vercel.app/",
     alt: "project 13"
   },
   {
     title: "Product Landing Page",
-    src: "https://drive.google.com/uc?id=19F5dVlRbPlXvkVtb_qFgRRVhx_UeZqCO",
+    src: "https://drive.google.com/uc?id=1dqDAvvXf_uKeIBDqIZT8UBNuIXrDbxWh",
     link: "https://cdpn.io/pen/debug/KKzqWjE?authentication_hash=jVApoNEwnRzr",
     alt: "project 14"
   },
   {
     title: "Survey Form",
-    src: "https://drive.google.com/uc?id=1SqxR02TuLxQq0boVQ1mLeD_t7OfDrN8d",
+    src: "https://drive.google.com/uc?id=1uu6Gah7hpvICzRR5dUJ0sqo7vS54kBQz",
     link: "https://cdpn.io/pen/debug/yLOXgeg?authentication_hash=XBrGRNjEvmBM",
     alt: "project 15"
   },
   {
     title: "Technical Documentation Page",
-    src: "https://drive.google.com/uc?id=1Gh8EQ2rkRDcw5zAoAdiEGQQbnh4L3dSK",
+    src: "https://drive.google.com/uc?id=1kWO4cfZeDZhPnrjiN2wkvvoTf3YmGibb",
     link: "https://cdpn.io/pen/debug/ZEWXEEQ?authentication_hash=LDkmdOnwewKA",
     alt: "project 16"
   },
   {
     title: "Tribute Page",
-    src: "https://drive.google.com/uc?id=11dGyQFrcONGIu2FUV0XsyNAT_L57i_QN",
+    src: "https://drive.google.com/uc?id=10UtPpP5OFeHW9VVYTH7m01HPNdWtFugG",
     link: "https://cdpn.io/pen/debug/XWdRBwo?authentication_hash=vWARwoGgZLJk",
     alt: "project 17"
   }
